8e43b3ebe4c34fda1b070b2225f98f72d82c1181
Isnan Nabawi <64667181+isnannabawi@users.noreply.github.com>
2022-12-06 08:50:41 +0700

    Add Calinski-Harabasz Index scorer
    Add looping for variating cluster size

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from datetime import datetime
 
 now = datetime.now()
 
-current_time = now.strftime("%H:%M:%S")
-print("Time Start = ", current_time)
+start_time = now.strftime("%H:%M:%S")
+print("Time Start = ", start_time)
 
 import matplotlib.pyplot as plt
 import csv
@@ -58,9 +58,9 @@ gsm_spec = np.array(gsm_spec)
 #18 n_internal_memory
 #19 n_ram
 #feature_selected = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
-feature_selected = [1,2,3,4,5,6,7,8,9,10,11,12]
+feature_selected = [2,4,16,17,19,13,18]
 
-print("\nFeature selected:")
+print("\nFeature used:")
 label_feature_selected = []
 for x in range (len(feature_selected)):
     label_feature_selected.append(gsm_spec_label[feature_selected[x]])
@@ -73,60 +73,87 @@ gsm_spec = preprocessing.minmax_scale(gsm_spec[:,feature_selected],axis=0)
 # Extract features
 #gsm_spec = gsm_spec[:, feature_selected]
 
-# Build SOM dimension
-som_m = 2
-som_n = 1
-som = SOM(m=som_m, n=som_n, dim=len(feature_selected), random_state=0)
-print('\nDimensions   = ', som_m, 'x', som_n)
-print('Cluster Size = ', som_m*som_n)
+#repeat for research
+p = 0
+q = 1
 
-# Fit it to the data
-som.fit(gsm_spec)
+print("\nDimensions\tRoom Size\tCluster Created\tSilhouette Score\tDavis Bouldin Score\tCalinski-Harabasz Index")
+report = ''
 
-# Assign each datapoint to its predicted cluster
-predictions = som.predict(gsm_spec)
-predictions = np.array(predictions)
+while p<10:
+    p += 1
+    q = 1
+    while q<5:
+        q += 1
+        # Build SOM dimension
+        som_m = p
+        som_n = q
+        som = SOM(m=som_m, n=som_n, dim=len(feature_selected), lr=1, max_iter = 10000, random_state=None)
+        # print('\nDimensions =', som_m, 'x', som_n)
+        # print('Room Size =', som_m*som_n)
 
-# Add predicted cluster column to table for result export
-ori_gsm_spec = np.c_[ori_gsm_spec,predictions]
+        report = str(som_m) + 'x' + str(som_n)
+        report = str(report) + '\t' + str(som_m*som_n)
 
-# Plot the results
-for d in range (len(feature_selected)):
-    y = predictions[:]
-   # y = gsm_spec[:,d-1]
-    x = gsm_spec[:,d]
-    colors = ['red', 'green', 'blue', 'lime', 'yellow','grey']
-    fig, ax = plt.subplots()
-    ax.scatter(x, y, c=predictions, cmap=ListedColormap(colors))
-    ax.title.set_text(gsm_spec_label[feature_selected[d]])
-    figname = 'result' + str(gsm_spec_label[feature_selected[d]])
-    plt.savefig(figname)
-#/////////////////////////
+        # Fit it to the data
+        som.fit(gsm_spec)
 
-# Export Result in CSV
-with open('result.csv', 'w', newline='', encoding='utf-8') as file:
-    writer = csv.writer(file, delimiter=',')
-    writer.writerow(["phone_id"] + label_feature_selected + ["cluster"]) # write header
-    writer.writerows(ori_gsm_spec)
+        # Assign each datapoint to its predicted cluster
+        predictions = som.predict(gsm_spec)
+        predictions = np.array(predictions)
 
-# Calculate Silhoutte Score
-from sklearn.metrics import silhouette_score
-score = silhouette_score(gsm_spec, predictions, metric='euclidean')
-print('\nSilhouetter Score: %.3f' % score)
+        # Add predicted cluster column to table for result export
+        ori_gsm_spec = np.c_[ori_gsm_spec,predictions]
+        #print('Cluster created =', max(predictions)+1)
+        report = report + '\t' + str(max(predictions)+1)
 
-# Calculate Davis Bouldin Score
-from sklearn.metrics import davies_bouldin_score
-score = davies_bouldin_score(gsm_spec, predictions)
-print('Davis Bouldin Score: %.3f' % score)
+        # Plot the results
+        # for d in range (len(feature_selected)):
+        #     y = predictions[:]
+        # # y = gsm_spec[:,d-1]
+        #     x = gsm_spec[:,d]
+        #     colors = ['red', 'green', 'blue', 'lime', 'yellow','grey']
+        #     fig, ax = plt.subplots()
+        #     ax.scatter(x, y, c=predictions, cmap=ListedColormap(colors))
+        #     ax.title.set_text(gsm_spec_label[feature_selected[d]])
+        #     figname = 'result' + str(gsm_spec_label[feature_selected[d]])
+        #     plt.savefig(figname)
+        #/////////////////////////
 
-# # Calculate Homogeneity Score
-# from sklearn import metrics
-# score = metrics.homogeneity_score(gsm_spec, predictions)
-# print('Homogeneity Score: %.3f' % score)
+        # Export Result in CSV
+        # with open('result.csv', 'w', newline='', encoding='utf-8') as file:
+        #     writer = csv.writer(file, delimiter=',')
+        #     writer.writerow(["phone_id"] + label_feature_selected + ["cluster"]) # write header
+        #     writer.writerows(ori_gsm_spec)
 
-# End of process
-now = datetime.now()
+        # Calculate Silhoutte Score
+        from sklearn.metrics import silhouette_score
+        score = silhouette_score(gsm_spec, predictions, metric='euclidean')
+        #print('Silhouette Score: %.3f' % score)
+        report = report + str('\t%.3f' % score)
 
-current_time = now.strftime("%H:%M:%S")
-print("\nTime Finished = ", current_time)
-print("Have a great day!")
+        # Calculate Davis Bouldin Score
+        from sklearn.metrics import davies_bouldin_score
+        score = davies_bouldin_score(gsm_spec, predictions)
+        #print('Davis Bouldin Score: %.3f' % score)
+        report = report + str('\t%.3f' % score)
+
+        # Calculate Calinski-Harabasz Index
+        from sklearn import metrics
+        from sklearn.metrics import pairwise_distances
+        score = metrics.calinski_harabasz_score(gsm_spec, predictions)
+        report = report + str('\t%.3f' % score)
+
+        # # Calculate Homogeneity Score
+        # from sklearn import metrics
+        # score = metrics.homogeneity_score(gsm_spec, predictions)
+        # print('Homogeneity Score: %.3f' % score)
+
+        # End of process
+        now = datetime.now()
+
+        current_time = now.strftime("%H:%M:%S")
+        #print("\nTime Finished = ", current_time)
+        #rint("Have a great day!\n")
+        print(report)
+
